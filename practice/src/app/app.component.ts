e0d83f680d6da4a336ad302c7c3266998e34c649
stm32p103 <stm32p103@gmail.com>
2020-07-09 21:57:09 +0900

    フルスクリーン表示

--- practice/src/app/app.component.ts
+++ practice/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit, OnDestroy } from '@angular/core';
+import { Application } from 'pixi.js'
+import { ResizeService } from './services/resize.service'
+import { Subject } from 'rxjs';
+import { map, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'practice';
+export class AppComponent implements AfterViewInit, OnDestroy {
+  private destroy = new Subject();
+  @ViewChild('containerCanvas') container;
+  pixi: Application;
+  constructor( private resize: ResizeService ){}
+
+  ngAfterViewInit() {
+    this.pixi = new Application( { view: this.container.nativeElement } );
+    this.pixi.renderer.resize( window.innerWidth, innerHeight );
+    this.resize.inner.pipe( map( size => this.pixi.renderer.resize( size.w, size.h ) ), takeUntil( this.destroy ) ).subscribe();
+  }
+
+  ngOnDestroy() {
+    this.destroy.next();
+  }
 }
--- practice/src/app/app.module.ts
+++ practice/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { ResizeService } from './services/resize.service';
 @NgModule({
   declarations: [
     AppComponent
@@ -10,7 +10,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [ResizeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
